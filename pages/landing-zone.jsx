4ee1cb6fff890763495573bd4bebf8ab0123047d
Oliver Parkinson <olliepar01@gmail.com>
2022-08-09 22:39:11 +0100

    getting params

--- pages/landing-zone.jsx
+++ pages/landing-zone.jsx
@@ -1,7 +1,13 @@
-const LandingZone = () => (
-  <>
-    <h1>loading</h1>
-  </>
-);
+import { useRouter } from "next/router";
+
+const LandingZone = () => {
+  const router = useRouter();
+  console.log({ router });
+  return (
+    <>
+      <h1>loading</h1>
+    </>
+  );
+};
 
 export default LandingZone;
