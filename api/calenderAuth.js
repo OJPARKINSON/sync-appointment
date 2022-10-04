925c7e52021416303a200a563a987941a1a1dc28
Oliver Parkinson <olliepar01@gmail.com>
2022-10-04 10:27:46 +0100

    I am sTuPiD

--- api/calenderAuth.js
+++ api/calenderAuth.js
@@ -2,8 +2,8 @@ const { google } = require("googleapis");
 const calendar = google.calendar("v3");
 
 const oauth2Client = new google.auth.OAuth2(
-  "123809003660-loa68504p4vkv03ce02g2fjrgg1s5543.apps.googleusercontent.com",
-  "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",
+  "_",
+  "_",
   "https://sync-appointment.vercel.app/landing-zone"
 );
 
